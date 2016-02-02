dc22728c904ad6254224df8c297369bc7d7e1cfc
tunnckoCore <mameto_100@mail.bg>
2016-02-02 13:36:57 +0200

    update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,26 +1,26 @@
 
 
-## v2.0.0 / March 26, 2015
+## 2.0.0 - 2015-03-26
 - Release v2.0.0 / npm@v2.0.0
 - **add** `related` section
 - **update** bump deps (notice `parse-github-short-url@v2.0.0`)
 
-## v1.0.4 / January 30, 2015
+## 1.0.4 - 2015-01-30
 - Release v1.0.4 / npm@v1.0.4
 - **update** rebuild readme
 - **update** bump `handle-errors`
 
-## v1.0.3 / January 28, 2015
+## 1.0.3 - 2015-01-28
 - Release v1.0.3 / npm@v1.0.3
 - **update** bump `parse-github-short-url@~1.0.5`
 - **add** more tests for exposed methods
 - **add** expose `parse-github-short-url` and his methods
 
-## v1.0.2 / January 28, 2015
+## 1.0.2 - 2015-01-28
 - Release v1.0.2 / npm@v1.0.2
 - **update** bump `handle-errors@^0.0.2`
 
-## v1.0.1 / January 25, 2015
+## 1.0.1 - 2015-01-25
 - Release v1.0.1 / npm@v1.0.1
 - **update** readme
 - **update** tests for refactored code
@@ -29,11 +29,11 @@
 - refactor
 - **add** more related
 
-## v1.0.0 / January 23, 2015
+## 1.0.0 - 2015-01-23
 - Release v1.0.0 / npm@v1.0.0
 - **add** keywords
 - **add** test: when array given
 - **add** docs
 
-## v0.0.0 / January 23, 2015
+## 0.0.0 - 2015-01-23
 - first commits
--- index.js
+++ index.js
@@ -1,117 +1,31 @@
-/**
+/*!
  * stringify-github-short-url <https://github.com/tunnckoCore/stringify-github-short-url>
  *
- * Copyright (c) 2014-2015 Charlike Mike Reagent, contributors.
+ * Copyright (c) 2015-2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
  * Released under the MIT license.
  */
 
-'use strict';
+'use strict'
 
-var fmt = require('util').format;
-var parse = require('parse-github-short-url');
-var errs = require('handle-errors')('stringify-github-short-url');
-
-/**
- * Expose `stringifyGithubShortUrl`
- */
-module.exports = stringifyGithubShortUrl;
-
-/**
- * Expose `parse-github-short-url` and his methods
- */
-Object.keys(parse).forEach(function(method) {
-  stringifyGithubShortUrl[method] = parse[method];
-});
-stringifyGithubShortUrl.parse = parse;
-
-/**
- * Stringify github short url object
- *
- * **Example:**
- *
- * ```js
- * var stringifyGithubShortUrl = require('stringify-github-short-url');
- * stringifyGithubShortUrl({
- *   user: 'tunnckoCore',
- *   username: 'tunnckoCore',
- *   org: 'tunnckoCore',
- *   organization: 'tunnckoCore',
- *   repo: 'glob2fp',
- *   repository: 'glob2fp',
- *   branch: 'master'
- * });
- * //=> 'tunnckoCore/glob2fp#master'
- *
- * stringifyGithubShortUrl({
- *   user: 'jonschlinkert',
- *   username: 'jonschlinkert',
- *   org: 'jonschlinkert',
- *   organization: 'jonschlinkert',
- *   repo: 'template',
- *   repository: 'template',
- *   branch: 'feature'
- * });
- * //=> 'jonschlinkert/template#feature'
- *
- * stringifyGithubShortUrl({
- *   user: 'visionmedia',
- *   username: 'visionmedia',
- *   org: 'visionmedia',
- *   organization: 'visionmedia',
- *   repo: 'mocha',
- *   repository: 'mocha',
- *   branch: ''
- * });
- * //=> 'visionmedia/mocha'
- * ```
- *
- * @name stringifyGithubShortUrl
- * @param  {Object} `<obj>` object to stringify
- * @param  {Function} `[cb]` callback for handle response
- * @return {String}
- * @api public
- */
-function stringifyGithubShortUrl(obj, cb) {
-  if (!obj) {
-    return errs.error('should have at least 1 argument');
+module.exports = function stringifyGithubShortUrl (owner, name, branch, npm) {
+  if (owner && typeof owner === 'object') {
+    var params = owner
+    owner = params.owner || params.user
+    name = params.name || params.repo
+    branch = params.branch
+    npm = params.npm
   }
 
-  if (typeOf(obj) !== 'object') {
-    return errs.type('expect `obj` (1st argument) be object', cb);
+  if (typeof owner !== 'string') {
+    throw new TypeError('stringify-github-short-url: expects `owner` to be a string')
+  }
+  if (typeof name !== 'string') {
+    return owner
   }
 
-  if (!parse.validate(obj)) {
-    return cb ? cb(null, '') : '';
+  var url = owner + '/' + name
+  if (typeof branch === 'string' && branch.length) {
+    return url + (npm === true ? '@' : '#') + branch
   }
-
-  var str = fmt('%s/%s', obj.user, obj.repo);
-
-  if (obj.branch) {
-    str = fmt('%s#%s', str, obj.branch);
-  }
-
-  var parsed = parse(str);
-
-  if (obj.user === parsed.user  && obj.repo === parsed.repo) {
-    return cb ? cb(null, str) : str;
-  }
-}
-
-/**
- * Get correct type of value
- *
- * @param  {*} `val`
- * @return {String}
- * @api private
- */
-function typeOf(val) {
-  if (typeof val !== 'object') {
-    return typeof val;
-  }
-
-  if (Array.isArray(val)) {
-    return 'array';
-  }
-
-  return {}.toString(val).slice(8, -1).toLowerCase();
+  return url
 }
